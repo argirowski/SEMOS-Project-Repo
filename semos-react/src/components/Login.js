108b01b75fe261d7d0518ab35c5894a76129c4ae
Elizzabeta <elizabetajovanovska48@gmail.com>
2018-12-27 15:34:31 +0100

    Reconstructed component's functionality

--- semos-react/src/components/Login.js
+++ semos-react/src/components/Login.js
@@ -1,161 +1,96 @@
-import React from "react";
-
-const Password = props => {
-  return (
-    <p>
-      <input
-        onChange={props.handleChange}
-        value={props.password}
-        name="password"
-        type="password"
-        placeholder="Password"
-      />
-    </p>
-  );
-};
-
-const Email = props => {
-  return (
-    <p>
-      <input
-        onChange={props.handleChange}
-        value={props.email}
-        name="email"
-        type="text"
-        placeholder="Email"
-      />
-    </p>
-  );
-};
+import React from 'react';
 
 export class Login extends React.Component {
+
   constructor(props) {
     super(props);
+
     this.state = {
-      email: "",
-      password: "",
-      errors: []
+      fields: {},
+      errors: {}
     };
+    this.sendData = this.sendData.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.submitForm = this.submitForm.bind(this);
-    this.changeInputType = this.changeInputType.bind(this);
-    this.onEmailChange = this.onEmailChange.bind(this);
-    this.onPasswordChange = this.onPasswordChange.bind(this);
-    this.clearValidationErr = this.clearValidationErr.bind(this);
   }
-  showValidationErr(element, msg) {
-    this.setState(prevState => ({
-      errors: [...prevState.errors, { element, msg }]
-    }));
-  }
-  // COLLAPSE ERROR WARNINGS // assistance needed
-  clearValidationErr(element) {
-    this.setState(prevState => {
-      let newArray = [];
-      for (let err of prevState.errors) {
-        if (element !== err.element) {
-          newArray.push(err);
-        }
-      }
-      return { errors: newArray };
-    });
-  }
-
-  onEmailChange(e) {
-    this.setState({ email: e.target.value });
-    this.clearValidationErr("email");
-  }
-
-  onPasswordChange(e) {
-    this.setState({ password: e.target.value });
-    this.clearValidationErr("password");
-  }
-
+ 
   handleChange(e) {
+    let fields = this.state.fields
+    fields[e.target.name] = e.target.value;
     this.setState({
-      [e.target.name]: e.target.value
+      fields
     });
   }
 
-  changeInputType() {
+  validateData(){
+    let fields = this.state.fields;
+    let errors = {};
+    let dataIsValid = true;
+
+    if(!fields["email"]) {
+      dataIsValid = false;
+      errors["email"] = "Please enter your e-mail";
+    }
+
+    if(typeof fields["email"] !== "undefined"){
+      let pattern = /\S+@\S+\.\S+/
+      if(!pattern.test(fields["email"])) {
+        dataIsValid = false;
+        errors["email"] = "Please enter valid e-mail";
+      }
+    }
+
+    if(!fields["password"]) {
+      dataIsValid = false;
+      errors["password"] = "Please enter your password";
+    }
+
+    if(typeof fields["password"] !== "undefined"){
+      let pattern = /(?=.{8,})/
+      if(!pattern.test(fields["password"])) {
+      dataIsValid = false;
+      errors["password"] = "Please enter valid password"
+      }
+    }
     this.setState({
-      type: this.state.type === "password" ? "text" : "password"
+      errors: errors
     });
+    return dataIsValid  
   }
 
-  submitForm(e) {
-    e.preventDefault();
-    if (
-      this.state.email === "" ||
-      this.state.email.indexOf("@") === -1 ||
-      this.state.email.indexOf(".com") === -1
-    ) {
-      this.showValidationErr("email", "Invalid e-mail.");
-    }
-    if (
-      this.state.password === "" ||
-      this.state.password.length < 8 ||
-      this.state.password > 32
-    ) {
-      this.showValidationErr("password", "Invalid password.");
-    }
-    let data = {
-      email: this.state.email,
-      password: this.state.password
-    };
-    console.log(data);
-  }
+
+  sendData(e) {
+  e.preventDefault(); 
+   if(this.validateData()) {
+     let fields = {};
+     fields["email"] = "";
+     fields["password"] = "";
+     this.setState({
+       fields:fields
+     });
+   }
+}
 
   render() {
-    let emailErr = null,
-      passwordErr = null;
-    for (let err of this.state.errors) {
-      if (err.element === "email") {
-        emailErr = err.msg;
-      }
-      if (err.element === "password") {
-        passwordErr = err.msg;
-      }
-    }
-
-    return (
+    return(
       <div className="log-in-box">
-        <div className="top-input-box">
-          <img
-            src={require("../assets/images/log-in.png")}
-            alt="login"
-            className="log-in-logo"
-          />
-          <h2>Log In</h2>
+      <div className = "top-input-box">
+      <img src = {require ("../assets/images/log-in.png")} alt="login" className="log-in-logo" />
+      <h2>Log In</h2>
 
-          <form onSubmit={this.submitForm}>
-            <div className="input-box">
-              <Email
-                handleChange={this.handleChange}
-                value={this.state.email}
-                onChange={this.onEmailChange}
-              />
-              <small className="error-warning">
-                {emailErr ? emailErr : ""}
-              </small>
-            </div>
-
-            <div className="input-box">
-              <Password
-                onChange={this.onPasswordChange}
-                value={this.state.password}
-                handleChange={this.handleChange}
-              />
-              <small className="error-warning">
-                {passwordErr ? passwordErr : ""}
-              </small>
-            </div>
-            <button onClick={this.submitForm} className="button" type="submit">
-              Log in
-            </button>
-          </form>
+        <form onSubmit={this.sendData}>
+        <div className="input-box">
+          <input value = {this.state.fields.email} onChange = {this.handleChange} placeholder="E-Mail" name = "email" type = "text" />
+          <div className="error-warning">{this.state.errors.email}</div>
         </div>
-      </div>
-    );
+
+        <div className="input-box">
+          <input value={this.state.fields.password} onChange = {this.handleChange} placeholder = "Password" name="password" type="text"/>
+          <div className="error-warning">{this.state.errors.password}</div>
+        </div>
+        <input onSubmit={this.resumeData} type="submit" value="GO!"/>
+        </form>
+        </div>
+        </div>
+    )
   }
 }
